103799fbb2fa183f2363def91f7bb503da469941
JJ-IM <20231294@daejin.ac.kr>
2024-03-17 03:50:07 +0000

    Models 수정 및 환경변수 추가

--- models/database.js
+++ models/database.js
@@ -1,38 +1,30 @@
-// Database 연결 코드
-//mongoose 와 monogdb 연결함
-
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-const {MONGO_ID, MONGO_PASSWORD, NODE_ENV} = process.env; // env 파일 만들어서 ID,PASSWORD 보호
-const mongoDbUrl = 'mongodb://eozkvnf:mnbvcxz098!@152.70.232.21:27017/carpool?authSource=user'; // 아이디, 비번 부분 ${} 형태로 바꿀 것
+const connectUserDb = () => {
+  const uri = process.env.USER_DB_URI; // 환경 변수 사용
+  const userDb = mongoose.createConnection(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  require('./User')(userDb); // User 모델 할당
 
-module.exports = () => {
-  const connect = () => {
-    if (NODE_ENV !== 'production') {
-      mongoose.set('debug', true);
-    }
-    mongoose.connect(mongoDbUrl, {
-      dbName : 'carpool',
-    }, (error) => {
-      if(error) {
-        console.log('mongodb 연결 에러', error);
-      } else {
-        console.log('mongodb 연결 성공');
-      }
-    });
-  };
-  connect();
-  
-  mongoose.connection.on('error', (error) => {
-    console.error('mongodb 연결 에러', error);
+  userDb.on('error', console.error.bind(console, 'User DB connection error:'));
+  userDb.once('open', () => {
+    console.log('Connected to User DB');
   });
-  mongoose.connection.on('disconected', ()=>{
-    console.error('mongodb 연결이 끊겼습니다. 연결을 재시도 합니다.');
-    connect();
-  })
 
-  require('./User');
-  require('./Post');
-  require('./chat');
-  require('./room');
+  return userDb;
 };
+
+const connectPostDb = () => {
+  const uri = process.env.POST_DB_URI; // 환경 변수 사용
+  const postDb = mongoose.createConnection(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  require('./Post')(postDb); // Post 모델 할당
+
+  postDb.on('error', console.error.bind(console, 'Post DB connection error:'));
+  postDb.once('open', () => {
+    console.log('Connected to Post DB');
+  });
+
+  return postDb;
+};
+
+module.exports = { connectUserDb, connectPostDb };
