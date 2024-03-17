74d06bebc024125d5638adce0d475de40e783e43
JJ-IM <20231294@daejin.ac.kr>
2024-03-17 09:10:23 +0000

    DB 연결 변경

--- models/database.js
+++ models/database.js
@@ -1,29 +1,26 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
 
 const connectUserDb = () => {
-  const uri = process.env.USER_DB_URI; // 환경 변수 사용
-  const userDb = mongoose.createConnection(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-  require('./User')(userDb); // User 모델 할당
+  const uri = 'mongodb://eozkvnf:vmfhwprxm12~@152.70.232.21:27017/User?authSource=admin';
+  const userDb = mongoose.createConnection(uri, {});
+  require('./User')(userDb);
 
   userDb.on('error', console.error.bind(console, 'User DB connection error:'));
   userDb.once('open', () => {
     console.log('Connected to User DB');
   });
-
   return userDb;
 };
 
 const connectPostDb = () => {
-  const uri = process.env.POST_DB_URI; // 환경 변수 사용
-  const postDb = mongoose.createConnection(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-  require('./Post')(postDb); // Post 모델 할당
+  const uri = 'mongodb://eozkvnf:vmfhwprxm12~@152.70.232.21:27017/Post?authSource=admin';
+  const postDb = mongoose.createConnection(uri, {});
+  require('./Post')(postDb);
 
   postDb.on('error', console.error.bind(console, 'Post DB connection error:'));
   postDb.once('open', () => {
     console.log('Connected to Post DB');
   });
-
   return postDb;
 };
 
